5e75deebcd8bdd0be3200d50b59e3f7b2a75d426
Danylo <vaganovd199@gmail.com>
2022-12-29 21:36:16 +0200

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("Converter for base converting");
+        System.out.println("Version 1.0");
     }
 }
