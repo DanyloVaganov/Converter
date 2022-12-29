1e424491dd34f46eead6ff4fe445d8a0a26cba53
Danylo <vaganovd199@gmail.com>
2022-12-29 21:49:21 +0200

    Change app info: short text

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
-        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
     }
 }
