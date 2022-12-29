390ec96a82fc6f09283f6c9e0a33b2f706fac45d
Danylo <vaganovd199@gmail.com>
2022-12-29 21:46:46 +0200

    Updatet up

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,11 +6,18 @@ public class Main {
         System.out.println("Converter for base converting");
         System.out.println("Version 1.0");
         double miles = 15;
+        double kms = 26;
         double kmsRes = convertMilesToKms(miles);
         System.out.println(kmsRes + " kms in " + miles + " miles.");
-    }
-        private static double convertMilesToKms(double miles) {
-            return miles * 1.60934;
-        }
+        double milesRes = convertKmsToMiles(kms);
+        System.out.println(milesRes + " miles in " + kms + " kms.");
     }
 
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
+    }
+
+    private static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
+    }
+}
